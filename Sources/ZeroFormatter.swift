1562e575ca4e1b4bbe92df058b8e946d8d5b05e3
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2016-11-24 01:28:17 +0900

    Add Xcode project

---
+++ Sources/ZeroFormatter.swift
@@ -0,0 +1,13 @@
+//
+//  ZeroFormatter.swift
+//  ZeroFormatter
+//
+//  Created by Yasuhiro Hatta on 2016/11/24.
+//  Copyright © 2016年 yaslab. All rights reserved.
+//
+
+import Foundation
+
+class ZeroFormatter: NSObject {
+
+}
---
+++ ZeroFormatter/ZeroFormatter.h
@@ -0,0 +1,19 @@
+//
+//  ZeroFormatter.h
+//  ZeroFormatter
+//
+//  Created by Yasuhiro Hatta on 2016/11/24.
+//  Copyright © 2016 yaslab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZeroFormatter.
+FOUNDATION_EXPORT double ZeroFormatterVersionNumber;
+
+//! Project version string for ZeroFormatter.
+FOUNDATION_EXPORT const unsigned char ZeroFormatterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZeroFormatter/PublicHeader.h>
+
+
