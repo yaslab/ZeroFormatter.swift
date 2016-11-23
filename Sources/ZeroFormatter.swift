e8f124d1fb9c57575411b81cce8ac8d8b9e231a8
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2016-11-24 01:37:20 +0900

    Update comment

--- Sources/ZeroFormatter.swift
+++ Sources/ZeroFormatter.swift
@@ -3,7 +3,7 @@
 //  ZeroFormatter
 //
 //  Created by Yasuhiro Hatta on 2016/11/24.
-//  Copyright © 2016年 yaslab. All rights reserved.
+//  Copyright © 2016 yaslab. All rights reserved.
 //
 
 import Foundation
